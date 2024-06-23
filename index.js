6a6956757828e8b96f1ac59771b02ed58c664429
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-06-23 21:16:18 +0200

    3.8: Phonebook backend step 8

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : null)
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
