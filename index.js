d468e3ba61d8fc430f56f7fe00b081bec49e1cf0
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-07-17 14:54:16 +0200

    3.9: Phonebook backend step 9 add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : null)
 
