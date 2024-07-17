a491d08fd7d65dda154bf753d21460d6988a5909
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-07-17 20:20:29 +0200

    3.11: phonebook step 11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : null)
 
