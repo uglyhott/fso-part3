16a14cdd915d981775dd1b06bc54fe91e70fd029
Liam Blumenstein-Tait <liam.bt@gmail.com>
2024-09-24 16:12:43 +0200

    3.16: phonebook database, step 4

--- index.js
+++ index.js
@@ -83,7 +83,17 @@ app.post('/api/persons', (req, res) => {
 
 })
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
